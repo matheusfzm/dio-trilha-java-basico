421b2dfe00f1d38d46743a04102ac8984639b7a9
Matheus Macedo <zanellamatheus15@gmail.com>
2024-06-06 11:37:33 -0300

    aula sobre tipos de variaveis e constantes

---
+++ tipos-variaveis/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //double salarioMinimo = 2500;
+
+
+
+        //short numeroCurto = 1;
+        //int numeroNormal = numeroCurto;
+        //short numeroCurto2 = (short) numeroNormal;
+
+        //int numero = 5;
+
+
+       
+    }
+}
