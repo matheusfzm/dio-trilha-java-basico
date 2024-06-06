abbb1a0c034f53ebaab490f84f6eb29dd9036dc2
Matheus Macedo <zanellamatheus15@gmail.com>
2024-06-06 18:22:47 -0300

    projeto-smarttv

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,33 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTV smartTV = new SmartTV();
+
+        smartTV.diminuirVolume();
+        smartTV.diminuirVolume();
+        smartTV.diminuirVolume();
+        smartTV.aumentarVolume();
+
+
+        System.out.println("Canal Atual: " + smartTV.canal);
+        smartTV.mudarCanal(13);
+        System.out.println("Canal Atual: " + smartTV.canal);
+
+        System.out.println("Volume Atual: " + smartTV.volume);
+
+        System.out.println("TV Ligada? " + smartTV.ligada);
+        
+        System.out.println("Volume Atual: " + smartTV.volume);
+
+
+        smartTV.ligar();
+        System.out.println("Novo Status -> TV Ligada? " + smartTV.ligada);
+
+        smartTV.desligar();
+        System.out.println("Novo Status -> TV Ligada? " + smartTV.ligada);
+
+
+
+
+    }
+}
