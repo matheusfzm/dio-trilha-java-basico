11eaa5d46e6dd4c8e1bc6c17a3bb8ad4235ac2d2
Matheus Macedo <zanellamatheus15@gmail.com>
2024-06-07 12:16:42 -0300

    FINAL-CURSO-JAVA-SINTAXE

---
+++ calculadora/src/Comentarios.java
@@ -0,0 +1,34 @@
+public class Comentarios {
+
+    public static void main(String[] args) {
+        //Comentario de uma unica linha
+
+        /*Olá eu sou um comentario
+         * para fim de teste e aprendizado
+         * de como deve ser feito um comentario 
+         * que utiliza varias linhas
+         */
+    }
+
+/*
+ * Este método foi elaborado as pressas
+ * por isso eu abrevei o nome das variáveis
+ * mas olha, ele tem a finalidade de somar ou  multiplicar
+ * dois números
+ */
+
+public int somaMultiplica (int n, int x, String m){
+    int r = 0; // r é igual ao resultado
+    if (m == "M"){ // M= multiplicação
+        r= n * x;
+    }else{
+        // se não soma mesmo
+        r = n + x;
+    }
+    return r;
+}
+         
+
+    }
+    
+
